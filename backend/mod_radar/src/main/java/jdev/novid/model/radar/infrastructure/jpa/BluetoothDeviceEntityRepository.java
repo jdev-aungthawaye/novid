9e3a7b3b15223a437d5c5a4b5a19ff7096589560
Aung Thaw Aye <aungthawaye@jdev.software>
2020-04-01 00:29:19 +0630

    formatted

--- backend/mod_radar/src/main/java/jdev/novid/model/radar/infrastructure/jpa/BluetoothDeviceEntityRepository.java
+++ backend/mod_radar/src/main/java/jdev/novid/model/radar/infrastructure/jpa/BluetoothDeviceEntityRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import jdev.novid.common.identity.LocationId;
 
 @Repository
-public interface BluetoothDeviceEntityRepository extends JpaRepository<BluetoothDeviceEntity, LocationId>{
+public interface BluetoothDeviceEntityRepository extends JpaRepository<BluetoothDeviceEntity, LocationId> {
 
 }
